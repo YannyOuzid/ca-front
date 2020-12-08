e98e23a83ec42fdc3cca0264ebc5c043913b5327
yannyiim <55101971+yannyiim@users.noreply.github.com>
2020-12-08 15:29:46 +0100

    album page

---
+++ album/js/script.js
@@ -0,0 +1,28 @@
+Amplitude.init({
+    "songs": [
+        {
+            "name": "Silience",
+            "artist": "Toonorth",
+            "album": "",
+            "url": "song/silience.mp3",
+        },
+        {
+            "name": "Get you the moon",
+            "artist": "Kina (ft. Snow)",
+            "album": "",
+            "url": "song/moon.mp3",
+        },
+        {
+            "name": "Goodnight ",
+            "artist": "RNLA (ft. DNAKM)",
+            "album": "",
+            "url": "song/good.mp3",
+        }
+    ],
+    callbacks: {
+
+        song_change: function () {
+            Amplitude.play();
+        }
+    }
+});
